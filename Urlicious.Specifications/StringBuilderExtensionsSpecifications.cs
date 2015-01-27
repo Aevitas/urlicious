c22b5284107d75ab1a6fb27dd2317b419e524615
aevitas <m4rtijn@gmail.com>
2015-01-27 13:39:08 +0100

    Added one more test for Trimming.

--- Urlicious.Specifications/StringBuilderExtensionsSpecifications.cs
+++ Urlicious.Specifications/StringBuilderExtensionsSpecifications.cs
@@ -21,4 +21,20 @@ namespace Urlicious.Specifications
         It stringBuilder_should_start_with_slash = () => _sb.StartsWith("/").ShouldBeTrue();
         It stringBuilder_should_end_with_slash = () => _sb.EndsWith("/").ShouldBeTrue();
     }
+
+    public class StringBuilderTrimSpecifications
+    {
+        private static StringBuilder _sb;
+
+        Establish context = () => _sb = new StringBuilder();
+
+        Because of = () =>
+        {
+            _sb.Append(";;;;SomeText456/////");
+            _sb.TrimStart(';');
+            _sb.TrimEnd('/');
+        };
+
+        It resulting_string_should_not_contain_semicolon_or_slashes = () => _sb.ToString().ShouldEqual("SomeText456");
+    }
 }
